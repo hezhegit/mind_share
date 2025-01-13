bba3769253c17bad165425ae6b2019e9be68a7a0
hezhe <3211384971@qq.com>
2025-01-13 19:17:12 +0800

    项目架构初始化

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"mind_share/dao/mysql"
+	"mind_share/dao/redis"
+	"mind_share/logger"
+	"mind_share/router"
+	"mind_share/setting"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("need config file.eg: bluebell config.yaml")
+		return
+	}
+	// 加载配置
+	if err := setting.Init(os.Args[1]); err != nil {
+		fmt.Printf("load config failed, err:%v\n", err)
+		return
+	}
+	if err := logger.Init(setting.Conf.LogConfig, setting.Conf.Mode); err != nil {
+		fmt.Printf("init logger failed, err:%v\n", err)
+		return
+	}
+	if err := mysql.Init(setting.Conf.MySQLConfig); err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
+	}
+	defer mysql.Close() // 程序退出关闭数据库连接
+	if err := redis.Init(setting.Conf.RedisConfig); err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+		return
+	}
+	defer redis.Close()
+
+	// 注册路由
+	r := router.SetupRouter(setting.Conf.Mode)
+	err := r.Run(fmt.Sprintf(":%d", setting.Conf.Port))
+	if err != nil {
+		fmt.Printf("run server failed, err:%v\n", err)
+		return
+	}
+}
---
+++ router/route.go
@@ -0,0 +1,29 @@
+package router
+
+import (
+	"mind_share/logger"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRouter 路由
+func SetupRouter(mode string) *gin.Engine {
+	if mode == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode) // gin设置成发布模式
+	}
+	r := gin.New()
+	//r.Use(logger.GinLogger(), logger.GinRecovery(true), middlewares.RateLimitMiddleware(2*time.Second, 1))
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "404",
+		})
+	})
+	return r
+}
